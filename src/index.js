e5f50ebd8e764a59991e64aae9dffc210e35e25f
Giuseppe Menti <mentifg@gmail.com>
2020-03-20 21:58:07 -0300

    Add firebase plugin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { firestore } from './plugins/firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +11,47 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+setImmediate(async () => {
+  // cria barbeiro
+  await firestore.collection('barbers').add({
+    name: 'Maria',
+    age: 18
+  });
+
+  // lista barbeiros
+  const result = await firestore.collection('barbers').get();
+  const barbers = [];
+  for (const doc of result.docs) {
+    barbers.push(doc.data());
+  }
+
+  // pega barbeiro
+  const result2 = await firestore
+    .collection('barbers')
+    .doc('s8ulDCqvaIkaAobSdOXx')
+    .get();
+
+  console.log(result2.data());
+
+  // atualiza barbeiro
+  await firestore
+    .collection('barbers')
+    .doc('s8ulDCqvaIkaAobSdOXx')
+    .update({ isUpdated: true });
+
+  console.log('atualizado');
+
+  // remove barbeiro
+  await firestore
+    .collection('barbers')
+    .doc('s8ulDCqvaIkaAobSdOXx')
+    .delete();
+
+  console.log('deletado');
+  
+  // pega localização
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log('my position ', position);
+  });
+});
