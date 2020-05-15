7be6f8a9a48a401626336b6a6eb20cde974e63e0
Giuseppe Menti <mentifg@gmail.com>
2020-05-15 20:50:07 -0300

    Move files to resource folder

---
+++ src/resources/Barber/BarberCreate.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  Create,
+  DateInput,
+  TextInput,
+  SelectInput,
+  FormTab,
+  TabbedForm,
+} from 'react-admin';
+
+import './styles.css';
+
+const BarberCreate = (props) => {
+
+  return (
+    <Create {...props}>
+      <TabbedForm>
+        <FormTab label='Perfil'>
+          <TextInput source='name' />
+          <TextInput source='email' type='email' />
+          <TextInput source='phone' />
+          <DateInput source='birthday' />
+
+          <SelectInput
+            source='transport'
+            choices={[
+              { id: 'Carro', name: 'Carro' },
+              { id: 'Moto', name: 'Moto' },
+            ]}
+          />
+        </FormTab>
+        <FormTab label='Endereço'>
+          <TextInput source='address.city' />
+          <TextInput source='address.district' />
+          <TextInput source='address.street' />
+          <TextInput source='address.number' />
+          <TextInput source='address.complement' />
+        </FormTab>
+      </TabbedForm>
+    </Create>
+  );
+};
+
+export default BarberCreate;
---
+++ src/resources/Barber/BarberEdit.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import {
+  Edit,
+  TabbedForm,
+  FormTab,
+  TextInput,
+  DateInput,
+  SelectInput,
+} from 'react-admin';
+
+const BarberEditTitle = ({ record }) => {
+  return <span>{record ? `${record.name}` : ''}</span>;
+};
+
+const BarberEdit = (props) => {
+  return (
+    <Edit {...props} title={<BarberEditTitle />} className='barber-list'>
+      <TabbedForm submitOnEnter={false}>
+        <FormTab label='Perfil'>
+          <TextInput source='name' />
+          <TextInput source='phone' />
+          <DateInput source='birthday' />
+          <SelectInput
+            source='transport'
+            choices={[
+              { id: 'Carro', name: 'Carro' },
+              { id: 'Carro', name: 'Moto' },
+            ]}
+          />
+          <TextInput source='email' />
+        </FormTab>
+
+        <FormTab label='Endereço'>
+          <FormTab label='Endereço'>
+            <TextInput source='address.city' />
+            <TextInput source='address.district' />
+            <TextInput source='address.street' />
+            <TextInput source='address.number' />
+            <TextInput source='address.complement' />
+          </FormTab>
+        </FormTab>
+      </TabbedForm>
+    </Edit>
+  );
+};
+
+export default BarberEdit;
