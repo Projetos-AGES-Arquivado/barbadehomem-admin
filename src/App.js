78c3af772b97833cab9a5e787e91d9b657f4ee8c
Giuseppe Menti <mentifg@gmail.com>
2020-05-15 20:54:54 -0300

    Add admin auth

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import { dataProvider } from './providers/firebase-provider';
 import { i18nProvider } from './providers/i18n-provider';
+import { authProvider } from './providers/auth-provider';
 
+import Login from './resources/Login';
 import Barber from './resources/Barber';
 
 const App = () => (
-  <Admin dataProvider={dataProvider} i18nProvider={i18nProvider}>
+  <Admin
+    dataProvider={dataProvider}
+    i18nProvider={i18nProvider}
+    authProvider={authProvider}
+    loginPage={Login}
+  >
     <Resource name='users' list={ListGuesser} />
     <Resource name='barbers' {...Barber} />
     <Resource name='barbers_addresses' />
---
+++ src/providers/auth-provider.js
@@ -0,0 +1,40 @@
+import 'firebase/auth';
+
+import * as firebase from 'firebase/app';
+import { firebaseConfig } from './firebase-provider';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export const authProvider = {
+  login: async ({ email, password }) => {
+    const authResult = await auth.signInWithEmailAndPassword(email, password);
+    const docUser = await firestore
+      .collection('users')
+      .doc(authResult.user.uid)
+      .get();
+    if (docUser.exists && docUser.get('isAdmin')) {
+      return authResult.user;
+    }
+    throw new Error('Permissão insuficiente');
+  },
+
+  logout: () => {
+    return auth.signOut();
+  },
+  checkError: (error) => {
+    throw error;
+  },
+  getPermissions: async () => [],
+  checkAuth: async () => {
+    const user = auth.currentUser;
+    if (user) {
+      return user;
+    }
+    throw new Error('Digite o seu login e senha');
+  },
+};
