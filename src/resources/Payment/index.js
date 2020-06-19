1e264ca7f88ad0c91c453ac3f96cd083ab783ad9
Arthur Lamberti <arthurlamberti001@gmail.com>
2020-06-18 23:55:54 -0300

    fix identacao

--- src/resources/Payment/index.js
+++ src/resources/Payment/index.js
@@ -4,8 +4,8 @@ import PaymentList from './PaymentList'
 import PaymentCreate from './PaymentCreate'
 import PaymentEdit from './PaymentEdit'
 export default {
-    list: PaymentList,
-    create:PaymentCreate,
-    edit: PaymentEdit,
-    icon:PaymentIcon
+  list: PaymentList,
+  create: PaymentCreate,
+  edit: PaymentEdit,
+  icon: PaymentIcon
 }
